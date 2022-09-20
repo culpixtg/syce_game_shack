6149ee6682bfd6d0d534f1a3bac72a1bc0d9dd19
Syce Gaming <77421128+SyceGaming@users.noreply.github.com>
2022-09-20 16:12:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let bannerMessageNum = "1"
+let bannerMessageNum = "2"
 const body = document.querySelector('body'),
         sidebar = body.querySelector('nav'),
         toggle = body.querySelector(".toggle"),
